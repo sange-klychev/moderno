edeb6a60a0826ea7ec496b71f3c147fad3c68c7d
sange-klychev <cheerius66@gmail.com>
2020-07-08 15:00:54 +0300

    added products-slider & product

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,19 @@
 $(function(){
     
+    $('.rate-star').rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+    });
+
     var mixer = mixitup('.products__inner-box');
+
+    $('.products-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4
+    });
 
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -34,6 +34,7 @@ gulp.task('script', function (){
         'node_modules/slick-carousel/slick/slick.js',
         'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
         'node_modules/mixitup/dist/mixitup.js',
+        'node_modules/rateyo/src/jquery.rateyo.js'
     ])
             .pipe(concat('libs.min.js'))
             .pipe(uglify())
@@ -44,7 +45,8 @@ gulp.task('style', function () {
     return gulp.src([
         'node_modules/slick-carousel/slick/slick.css',
         'node_modules/normalize.css/normalize.css',
-        'node_modules/magnific-popup/dist/magnific-popup.css'
+        'node_modules/magnific-popup/dist/magnific-popup.css',
+        'node_modules/rateyo/src/jquery.rateyo.css'
     ])
         .pipe(concat('libs.min.css'))
         .pipe(cssmin())
