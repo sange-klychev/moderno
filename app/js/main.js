ef949a4151b9bee3f084e8165abb96acf2de55ac
sange-klychev <cheerius66@gmail.com>
2020-07-24 16:12:05 +0300

    added page upload

--- app/js/main.js
+++ app/js/main.js
@@ -39,14 +39,16 @@ $(function(){
         $('.header__box').toggleClass('active');
     });
 
-    $('.product-one__tabs, .settings__tabs .tab').on('click', function (event) {
+    $('.tabs, .tab').on('click', function (event) {
         var id = $(this).attr('data-id');
         $('.product-one__tabs, .settings__tabs').find('.tab-item').removeClass('active-tab').hide();
-        $('.product-one__tabs, .settings__tabs .tabs').find('.tab').removeClass('active');
+        $('.product-one__tabs, .settings__tabs, .tabs').find('.tab').removeClass('active');
         $(this).addClass('active');
         $('#' + id).addClass('active-tab').fadeIn();
         return false;
     });
+
+    $('input[type=file], select').styler();
 
     if ($('.products__block').length) {
     var mixer = mixitup('.products__inner-box');
